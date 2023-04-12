8113e6b2dabea0b88e20f5f23f9bd556241aa1a5
abikosik3000 <Padla-Oleg3000@yandex.ru>
2023-04-12 17:05:44 +0300

    add ini file

--- database/connection_db.php
+++ database/connection_db.php
@@ -1,16 +1,14 @@
 <?php
-$DBuser = 'root';
-$DBpass = $_ENV['MYSQL_ROOT_PASSWORD'];
+$ini = parse_ini_file('../.ini');
+$DBuser = $ini["db_user"];
+$DBpass = $ini["db_password"];
 $GLOBALS['pdo'] = null;
-
 try{
     global $pdo;
-    $database = 'mysql:host=database:' . $_ENV['HOST_MACHINE_MYSQL_PORT'];
+    $database = $ini["db_for_pdo"];
     $pdo = new PDO($database, $DBuser, $DBpass);
-    $pdo->query("USE test_from_admin");
+    $pdo->query("USE {$ini["db_basename"]}");
     //echo "Success: A proper connection to MySQL was made! The docker database is great.";    
 } catch(PDOException $e) {
     echo "Error: Unable to connect to MySQL. Error:\n $e";
 }
-
-//DROP OBJECT_TYPE [ IF EXISTS ] OBJECT_NAME
